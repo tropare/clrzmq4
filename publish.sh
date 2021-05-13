13731f734e79e4d9905b46d5366275c77b9b3afb
Mark Punak <mark@tropare.com>
2021-05-12 18:13:49 -0600

    tropare github package repo publish script

--- publish.sh
+++ publish.sh
@@ -1,29 +1,53 @@
 #/bin/bash
+#------------------------------------------------------
+# ----                READ BEFORE RUNNING!!!!
+#------------------------------------------------------
 
-# READ BEFORE RUNNING!!!!
-# This script relies on you having a GITHUB_PAT variable set in your environment.
-# The GITHUB_PAT is your GITHUB personal access token with rights to publish and read packages from github
-# This can be created  in your github settings->developer settings and then set in your environment by running:
-#    export GITHUB_PAT=<your personal acccess token>
+# Your git environment must be configured for package management before any scriptlet in this script will work.
+# 1. Generate a package read/write enabled personal access token (PAT) in the Github Settings screen for your user
+#       This can be created  in your github settings->developer settings 
+# 2. Modify your ~/.bash_profile script to export your Github PAT  as an environment variable
+#       export GITHUB_PAT=<your personal acccess token>
+# 3. Make sure your ~/.nuget/NuGet/Nuget.Config has the Github Tropare Package repo added as the first source for packages, above the NugetRepo
+
+# ---------------- Github Package Repo Management ----------------------
+# -- delete --- WARNING -- Github doesnt support commandline package removal. You must use the Git Web ui to remove packages ---
+#dotnet nuget delete ZeroMQ 4.1.0.32-tropare -s github --non-interactive -k $GITHUB_PAT
+
+# -- clean
+dotnet clean ZeroMQ.netcore.csproj
+rm -rf ./nupkg
+
+# -- pack
+dotnet pack ZeroMQ.netcore.csproj -p:NuspecFile=ZeroMQ.tropare.nuspec -p:PackageOutputPath=./nupkg --configuration Release
+#dotnet pack -v n -p:RepositoryUrl=https://github.com/tropare/vdf-transform -p:RepositoryType=git
+
+# -- publish
+dotnet nuget push nupkg/*.nupkg -s github  -k $GITHUB_PAT
+
+
+# -- consuume
+dotnet add package ZeroMQ --version 4.1.0.31-tropare
+
+# ---------------- Github Package Repo Management ----------------------
+
+
 
 #dotnet restore src/NetMQ
 # dotnet build ZeroMQ.netcore.csproj --configuration Release
 # donet build ZeroMQ.netcore.csproj -p:Version=4.1.0.33-tropare --configuration Release
 # dotnet pack ZeroMQ.netcore.csproj -p:Version=4.1.0.33-tropare -p:NuspecFile=ZeroMQ.nuspec --configuration Release
 
-dotnet pack ZeroMQ.netcore.csproj -p:NuspecFile=ZeroMQ.tropare.nuspec --configuration Release
-dotnet nuget push ./bin/Release/ZeroMQ.4.1.0.33-tropare.nupkg -s https://nuget.pkg.github.com/tropare/index.json -k $GITHUB_PAT
 
 
-# OBSOLETE - Delete local package repo item
-# dotnet nuget delete NetMQ 4.1.0.32-tropare -s ~/.nuget/packages/ --non-interactive
+
 
 
 # -------- Dotnet package manager tool installation instructions -----
 # - Build tool package
 #dotnet pack
 #dotnet pack -p:NuspecFile=msg-bus.nuspec 
-#dotnet pack -v n -p:RepositoryUrl=https://github.com/tropare/vdf-transform -p:RepositoryType=git
+
 
 # - Uninstall a tool
 #dotnet -d tool uninstall msg-bus
